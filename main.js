6ae68b86770dedd0c2243fdf9beccba5cc87ecc9
Ghost <108940365+vinibr33@users.noreply.github.com>
2024-06-13 09:43:53 -0300

    Aula 13/06

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
+const contador = document.querySelectorAll(".contador");
 for (let i = 0 ; i < botoes.length ; i++){
     botoes[i].onclick = function (){
         for (let j = 0; j < botoes.length ; j++){

a04993c6518e79a642aac5229141af2aaff08c3c
Ghost <108940365+vinibr33@users.noreply.github.com>
2024-06-14 08:55:25 -0300

    Aula 14/06

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const contador = document.querySelectorAll(".contador");
+contador[0].textContent = "Olá"
+const tempoObjetivo1 = new Date(2024,11,31,23,59)
+const tempoObjetivo2 = new Date(2024,11,31,23,59)
+const tempoObjetivo3 = new Date(2024,11,31,23,59)
+const tempoObjetivo4 = new Date(2024,11,31,23,59)
 for (let i = 0 ; i < botoes.length ; i++){
     botoes[i].onclick = function (){
         for (let j = 0; j < botoes.length ; j++){

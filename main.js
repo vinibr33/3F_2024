65c2ab15384985c60ee59dfb3d321b1d126d1e6d
Ghost <108940365+vinibr33@users.noreply.github.com>
2024-06-27 09:47:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,27 @@ const tempoObjetivo1 = new Date(2024,11,31,23,59)
 const tempoObjetivo2 = new Date(2024,11,31,23,59)
 const tempoObjetivo3 = new Date(2024,11,31,23,59)
 const tempoObjetivo4 = new Date(2024,11,31,23,59)
+
+function calculaTempo(tempoObjetivo){
+    const agora = new Date()
+    let segundos
+    let minutos
+    let horas
+    let dias
+    segundos = (tempoObjetivo - agora)/1000
+    minutos = segundos/60
+    horas = minutos/60
+    dias = horas/24
+    segundos = Math.floor(segundos)
+    minutos = Math.floor(minutos)
+    horas = Math.floor(horas)
+    dias = Math.floor(horas)
+
+    segundos = segundos%60
+    minutos = minutos%60
+    horas = horas%24
+}
+
 let agora = new Date();
 let segundos 
 segundos = (tempoObjetivo1-agora)/1000;
@@ -15,6 +36,13 @@ let horas
 horas = minutos/60
 let dias
 dias = horas/24
+segundos = Math.floor(segundos);
+minutos = Math.floor(minutos);
+horas = Math.floor(horas);
+dias = Math.floor(dias);
+segundos = segundos%60;
+minutos = minutos%60;
+horas = horas%24;
 contador[0].textContent = `faltam ${dias} dias, ${horas}
 horas, ${minutos} minutos e ${segundos} segundos`
 contador[1].textContent = tempoObjetivo2-agora

66e4d8252191c1359480ca8a7425accb127ef45c
Ghost <108940365+vinibr33@users.noreply.github.com>
2024-08-30 09:02:12 -0300

    Aula 30/08

--- main.js
+++ main.js
@@ -42,9 +42,9 @@ function calculaTempo(tempoObjetivo) {
 }
 
 function atualizaCronometro(){
-       
+    document.getElementById('dias0').textContent = calculaTempo(tempos[0])
     for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);  
+      //  contadores[i].textContent = calculaTempo(tempos[i]);  
     }
 }
 

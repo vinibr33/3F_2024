f48f230f1b1cca0b9f5151e9b0257fc8e953c971
Ghost <108940365+vinibr33@users.noreply.github.com>
2024-10-10 09:43:06 -0300

    Aula 10/10

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# 3F_2024
+# 3F-2Projeto
+Este repositório funcionará como um bom lugar que sempre poderemos contar para salvar nossos arquivos '-'
---
+++ quantidadeDeUsuarios.js
@@ -0,0 +1,16 @@
+
+async function quantidadeDeUsuarios(){
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json();
+    const nomeDasRedes = Object.keys(dados);
+    const quantidadeDeUsuarios = Object.values(dados);
+
+    const infos = [
+        {
+            x: nomeDasRedes,
+            y: quantidadeDeUsuarios,
+            type: 'bar'
+        }
+    ]
+}
